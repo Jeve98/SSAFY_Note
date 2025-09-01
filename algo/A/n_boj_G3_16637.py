d9de6abe5ae7d943f44e3399e3ffab1d05e169f3
YeonWookJeon <jadam41817@gmail.com>
2025-09-01 16:41:23 +0900

    computingThink

---
+++ algo/A/n_boj_G3_16637.py
@@ -0,0 +1,20 @@
+# 연산자 우선순위가 없기 때문에 처음과 마지막에서는 괄호가 들어가지 않아도 됨
+# length - 2(처음-연산자/피연산자) - 2(마지막-연산자/피연산자) // 2 개의 괄호
+
+length = int(input())   # 1 <= length <= 19 (최대 7! - 5040, 0.5초 아슬아슬하게 될지도?)
+expression = input()
+
+
+def permutation():
+    pass
+
+
+def calculation():
+    pass
+
+
+maxCal = -2100000000
+
+permutation()
+
+print(maxCal)
