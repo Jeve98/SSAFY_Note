ca1713c85270e763e90816a7a645773133a4954b
YeonWookJeon <jadam41817@gmail.com>
2025-11-24 16:46:46 +0900

    course

---
+++ note/11_November/251121.py
@@ -0,0 +1,17 @@
+"""
+[251121]
+
+<JS 라이브 강의>
+
+Input Event : 요소의 value가 변경되거나 붙여넣기에도 반응하는 유효성 검사 기능
+
+
+<GMS-API>
+
+GMS(Gen AI Management System) : 서로 다른 생성형 AI 서비스를 하나의 API로 통합해주는 플랫폼
+
+
+<실습>
+
+
+"""
