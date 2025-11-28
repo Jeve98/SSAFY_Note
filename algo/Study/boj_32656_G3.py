6401d0727d31ab1478feb3698d48f20d5b5b2fcd
YeonWookJeon <jadam41817@gmail.com>
2025-11-28 17:19:28 +0900

    algo

--- algo/Study/boj_32656_G3.py
+++ algo/Study/boj_32656_G3.py
@@ -2,51 +2,54 @@ import sys
 
 sys.stdin = open("algo/input.txt", "r")
 
+
+def DFS(start, end):
+    if start == end:
+        return []
+
+    stack = [0] * (vertexC + 1)
+    top = -1
+
+    visited = [False] * (vertexC + 1)
+
+    top += 1
+    stack[top] = start
+    visited[start] = True
+
+    tmp = [start]
+    isReturn = False
+    while top != -1:
+        for nextNode in edgeList[stack[top]]:
+            if nextNode == end:
+                isReturn = True
+                visited[nextNode] = True
+            elif not visited[nextNode]:
+                top += 1
+                stack[top] = nextNode
+                visited[nextNode] = True
+
+                tmp.append(nextNode)
+                break
+        else:
+            top -= 1
+    print(tmp)
+
+    if isReturn:
+        return tmp
+
+
 vertexC = int(input())
 
-edgeList = [[] for _ in range(vertexC)]
+edgeList = [set() for _ in range(vertexC + 1)]
 for i in range(vertexC - 1):
     data = list(map(int, input().split()))
-    edgeList[data[0] - 1].append(data[1] - 1)
-    edgeList[data[1] - 1].append(data[0] - 1)
+    edgeList[data[0]].add(data[1])
+    edgeList[data[1]].add(data[0])
 
-lca = list(map(int, input().split()))
+lca = list(map(int, input().split()))  # 0: child, 1: child, 2: parents
 
+route = []
+for i in range(2):
+    route.extend(DFS(lca[i], lca[2]))
 
-def make_set(x):
-    parents = [i for i in range(x + 1)]
-    ranks = [0] * (x + 1)
-    return parents, ranks
-
-
-def find_set(x):
-    if x == parents[x]:
-        return x
-
-    parents[x] = find_set(parents[x])
-
-    return parents[x]
-
-
-def union(x, y):
-    repX = find_set(x)
-    repY = find_set(y)
-
-    if repX == repY:
-        return
-
-    if ranks[repX] < ranks[repY]:
-        parents[repX] = repY
-    elif ranks[repX] > ranks[repY]:
-        parents[repY] = repX
-    else:
-        parents[repY] = repX
-        ranks[repX] += 1
-
-
-parents, ranks = make_set(vertexC)
-for i in range(vertexC - 1):
-    for j in edgeList[i]:
-        union(i, j)
-
-print(parents)
+print(vertexC - len(route))
