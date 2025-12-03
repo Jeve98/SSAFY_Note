c50fb1936b99fa5dd7259045511f0c242934c9b8
Jeve98 <jadam41817@gmail.com>
2025-12-03 11:53:57 +0900

    course

---
+++ vue/vue-project/src/components/GrandChild.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        grand child
+        message : {{ msg }}
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        msg: String,
+    })
+</script>
+
+<style scoped>
+
+</style>
